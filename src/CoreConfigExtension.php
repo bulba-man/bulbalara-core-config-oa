01a623e0223d2406ecd3d329ebd43f55adbe1ce7
Mikita Dzmitryievich <nikitadm@tut.by>
2025-06-14 18:06:09 +0300

    fix mail config merging

--- src/CoreConfigExtension.php
+++ src/CoreConfigExtension.php
@@ -32,9 +32,11 @@ class CoreConfigExtension extends Extension
 
         $mailer = config('mail.transport.mailer');
         config(['mail.default' => $mailer]);
-        config(['mail.mailers.'.$mailer => array_merge(
-            config('mail.mailers.'.$mailer), config('mail.transport.'.$mailer)
-        )]);
+        if (config('mail.transport.'.$mailer)) {
+            config(['mail.mailers.'.$mailer => array_merge(
+                config('mail.mailers.'.$mailer), config('mail.transport.'.$mailer)
+            )]);
+        }
 
         config(['mail.from' => config('mail.addresses.from')]);
     }
